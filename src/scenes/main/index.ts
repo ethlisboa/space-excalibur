19a0b506474413afecc6b754b2d62f9d635c588b
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-10-22 22:56:31 +0100

    scenes: allow avatar to move around

--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -1,17 +1,52 @@
 import { Scene } from 'phaser';
 
 export class BaseMapScene extends Scene {
+  private avatar?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor() {
     super('map-scene');
   }
 
   preload() {
     this.load.image('stars', 'assets/bg/stars.png');
+    this.load.image('avatar', 'assets/sprites/avatar.png')
   }
 
   create(): void {
-    console.log(this.game.canvas.width, this.game.canvas.height);
-    this.add.tileSprite(0, 0, this.game.canvas.width * 2, this.game.canvas.height * 2, 'stars').setOrigin(0)
+
+    // Input Keys
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Background
+    this.add.tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, 'stars')
+      .setOrigin(0)
       .setScrollFactor(0, 1);
+
+    // Avatar
+    this.avatar = this.physics.add.sprite(150, 120, 'avatar');
+    this.avatar.setGravity(0, 0);
+  }
+
+  update(): void {
+
+    // Avatar Movement (Horizontal)
+    let speed = 160;
+    if (this.cursors?.left.isDown) {
+      this.avatar?.setVelocityX(-speed);
+    } else if (this.cursors?.right.isDown) {
+      this.avatar?.setVelocityX(speed);
+    } else {
+      this.avatar?.setVelocityX(0);
+    }
+
+    // Avatar Movement (Vertical)
+    if (this.cursors?.up.isDown) {
+      this.avatar?.setVelocityY(-speed);
+    } else if (this.cursors?.down.isDown) {
+      this.avatar?.setVelocityY(speed);
+    } else {
+      this.avatar?.setVelocityY(0);
+    }
   }
 }
