4ace3bfc77bcea5cafa4381eb0b5ff6cab96d382
Marin Petrunić <mpetrunic@users.noreply.github.com>
2021-10-22 21:09:37 +0100

    initial structure (#3)
    * initial structure
    
    * move some stuff

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { Game, Types } from 'phaser';
+
+import { LoadingScene } from './scenes';
+
+declare global {
+    interface Window { 
+        sizeChanged: () => void;
+        game: Phaser.Game;
+     }
+}
+
+const gameConfig: Types.Core.GameConfig = {
+	title: 'Phaser game tutorial',
+  type: Phaser.WEBGL,
+  parent: 'game',
+  backgroundColor: '#351f1b',
+  scale: {
+    mode: Phaser.Scale.ScaleModes.NONE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+  render: {
+    antialiasGL: false,
+    pixelArt: true,
+  },
+  callbacks: {
+    postBoot: () => {
+      window.sizeChanged();
+    },
+  },
+  canvasStyle: `display: block; width: 100%; height: 100%;`,
+  autoFocus: true,
+  audio: {
+    disableWebAudio: false,
+  },
+  scene: [LoadingScene],
+};
+
+window.sizeChanged = () => {
+    if (window.game.isBooted) {
+      setTimeout(() => {
+        window.game.scale.resize(window.innerWidth, window.innerHeight);
+  
+        window.game.canvas.setAttribute(
+          'style',
+          `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
+        );
+      }, 100);
+    }
+  };
+  
+window.onresize = () => window.sizeChanged();
+
+window.game = new Game(gameConfig);
