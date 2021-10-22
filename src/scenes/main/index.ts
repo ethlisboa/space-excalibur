c00e7203f8cda399d04776f94e29632c9b207695
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-10-23 00:37:58 +0100

    scene: add debug tile position coordinates (#7)

--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -5,6 +5,10 @@ export class BaseMapScene extends Scene {
   private avatar?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
+  private isDebugging = true;
+  private debugText?: Phaser.GameObjects.Text;
+  private debugWelcome = "SpaceXcalibur\n";
+
   constructor() {
     super('map-scene');
   }
@@ -20,13 +24,23 @@ export class BaseMapScene extends Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Background
-    this.background = this.add.tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, 'stars')
+    this.background = this.add
+      .tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, 'stars')
       .setOrigin(0)
       .setScrollFactor(0, 1);
 
     // Avatar
-    this.avatar = this.physics.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'avatar');
+    this.avatar = this.physics.add.sprite(
+      this.game.canvas.width / 2,
+      this.game.canvas.height / 2,
+      'avatar'
+    );
     this.avatar.setGravity(0, 0);
+
+    // Debugging
+    if (this.isDebugging) {
+      this.debugText = this.add.text(0, 0, this.debugWelcome + this.getPositionXY())
+    }
   }
 
   update(): void {
@@ -45,5 +59,14 @@ export class BaseMapScene extends Scene {
     } else if (this.background && this.cursors?.down.isDown) {
       this.background.tilePositionY += speed;
     }
+
+    // Debugging
+    if (this.isDebugging) {
+      this.debugText?.setText(this.debugWelcome + this.getPositionXY())
+    }
+  }
+
+  getPositionXY(): string {
+    return "TilePosition: " + this.background?.tilePositionX.toString() + "," + this.background?.tilePositionY.toString();
   }
 }
