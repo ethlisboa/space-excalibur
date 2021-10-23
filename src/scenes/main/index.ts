ae7ac12ba0472308f2168a930300de25f207ea1b
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-10-23 12:42:31 +0100

    scene: add debug container

--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -6,8 +6,12 @@ export class BaseMapScene extends Scene {
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
   private isDebugging = true;
+  private debugContainer?: Phaser.GameObjects.Container;
+  private debugBackground?: Phaser.GameObjects.Rectangle;
   private debugText?: Phaser.GameObjects.Text;
-  private debugWelcome = "SpaceXcalibur\n";
+  private debugWelcome = "Nym: Arthur";
+  private debugFontStyle = { font: "10px Mono", fill: "#fca" };
+  private gameTick = 0;
 
   constructor() {
     super('map-scene');
@@ -26,64 +30,86 @@ export class BaseMapScene extends Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Background
-    // this.background = this.add
-    //   .tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, 'stars')
-    //   .setOrigin(0)
-    //   .setScrollFactor(0, 1);
-
     this.map = this.make.tilemap({key: 'map', tileWidth: 50, tileHeight: 50});
     const spaceTileset = this.map.addTilesetImage('space', 'space-tileset')
     const planetsTileset = this.map.addTilesetImage('planet', 'planet')
     const spaceLayer = this.map.createLayer("space", spaceTileset, 0, 0);
     const planetLayer = this.map.createLayer("planets", planetsTileset, 0, 0);
-
-
     this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+
     // Avatar
     this.avatar = this.physics.add.sprite(
       this.game.canvas.width / 2,
       this.game.canvas.height / 2,
       'avatar'
     );
-
     this.avatar.setGravity(0, 0);
     this.avatar.setCollideWorldBounds(true);
+    this.avatar.setInteractive({useHandCursor: true});
+    this.avatar.on('pointerdown', () => {
+      this.isDebugging = !this.isDebugging;
+    });
 
-    this.cameras.main.startFollow(this.avatar, true)
+    // Camera
+    this.cameras.main.startFollow(this.avatar, true);
     this.cameras.main.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels, true);
 
     // Debugging
     if (this.isDebugging) {
-      this.debugText = this.add.text(0, 0, this.debugWelcome + this.getPositionXY())
+      this.debugText = this.add.text(0, 0, "nil", this.debugFontStyle);
+      this.debugBackground = this.add.rectangle(0,0,0,0,0x000);
+      this.debugContainer = this.add.container(0, 0);
+      this.debugContainer.setSize(160, 120);
+      this.debugContainer.add(this.debugBackground)
+      this.debugContainer.add(this.debugText);
+      this.debugContainer.setAlpha(0.5);
     }
   }
 
   update(): void {
+    this.gameTick++;
 
-    // Avatar Movement 
+    // Avatar Movement
+    const movementSpeed = 100;
     this.avatar?.setVelocityX(0);
     this.avatar?.setVelocityY(0);
-
     if(this.cursors?.up.isDown === true) {
-      this.avatar?.setVelocityY(-100);
+      this.avatar?.setVelocityY(-movementSpeed);
     }
     if (this.cursors?.down.isDown === true) {
-      this.avatar?.setVelocityY(100);
+      this.avatar?.setVelocityY(movementSpeed);
     }
     if (this.cursors?.left.isDown === true) {
-      this.avatar?.setVelocityX(-100);
+      this.avatar?.setVelocityX(-movementSpeed);
     }
     if (this.cursors?.right.isDown === true) {
-      this.avatar?.setVelocityX(100);
+      this.avatar?.setVelocityX(movementSpeed);
     }
 
     // Debugging
+    this.debugContainer?.setVisible(this.isDebugging);
     if (this.isDebugging) {
-      this.debugText?.setText(this.debugWelcome + this.getPositionXY())
+      this.debugText?.setText(
+        this.debugWelcome
+        + "\nPos: "
+        + this.getPositionXY()
+        + "\nAge: "
+        + this.gameTick
+      );
+      this.debugContainer?.setPosition(
+        this.avatar?.getBottomRight().x,
+        this.avatar?.getBottomRight().y
+      );
+      if (this.debugText) {
+        this.debugBackground?.setSize(this.debugText.displayWidth, this.debugText.displayHeight);
+      }
     }
   }
 
   getPositionXY(): string {
-    return "TilePosition: " + this.avatar?.getCenter().x.toString() + "," + this.avatar?.getCenter().y.toString();
+    return ""
+      + this.avatar?.getTopLeft().x.toFixed(0).toString()
+      + ","
+      + this.avatar?.getTopLeft().y.toFixed(0).toString();
   }
 }
