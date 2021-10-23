04071a3c9831a0c6b6a065e64459ff1bf80dc24f
Marin <marin.petrunic@gmail.com>
2021-10-23 17:53:20 +0100

    web3provider

--- src/actions/web3.ts
+++ src/actions/web3.ts
@@ -1,16 +1,23 @@
 import Web3Modal, { IProviderOptions } from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+import { Web3Provider } from "@ethersproject/providers";
+import { providers } from "ethers";
 
 const opts: IProviderOptions = {
     walletconnect: {
         package: WalletConnectProvider, // required
         options: {
-          infuraId: "fd1f29ab70844ef48e644489a411d4b3" // required
+            infuraId: "fd1f29ab70844ef48e644489a411d4b3" // required
         }
-      }
+    }
 }
 
 export const web3Modal = new Web3Modal({
     cacheProvider: true, // optional
     providerOptions: opts // required
 });
+
+
+export async function getWeb3Provider(): Promise<Web3Provider> {
+    return new providers.Web3Provider(await web3Modal.connect())
+}
--- src/scenes/util/menu.ts
+++ src/scenes/util/menu.ts
@@ -15,15 +15,27 @@ export class MenuScene extends Scene {
     create(): void {
         const connectButton = new Button(this, this.sys.game.canvas.width - 200, 50, "Connect");
         connectButton.onClick(async () => {
+            await web3Modal.clearCachedProvider();
             const web3Provider = await web3Modal.connect();
             const provider = new providers.Web3Provider(web3Provider);
-            if(await (await provider.getNetwork()).chainId !== 69) {
+            if (await (await provider.getNetwork()).chainId !== 69) {
                 window.alert("Unsupported network. Only Optimistic Kovan is supported");
                 return;
             }
             const acc = await (await provider.getSigner()).getAddress();
             connectButton.setText(acc);
             connectButton.disable();
+            web3Provider.on("disconnect", () => {
+                connectButton.setText("Connect")
+                connectButton.enable();
+            })
+            web3Provider.on("accountsChanged", (accounts: any) => {
+                if(accounts.length === 0) {
+                    connectButton.setText("Connect")
+                    connectButton.enable();
+                }
+                connectButton.setText(accounts[0]);
+            });
         })
         this.add.existing(connectButton);
     }
