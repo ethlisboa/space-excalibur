d8dca7c7a547fa1c1795aa887007e62e4a857161
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-10-23 23:58:39 +0100

    planets: enable menu container on click (#23)

--- src/scenes/main/planets/menu.ts
+++ src/scenes/main/planets/menu.ts
@@ -1,11 +1,26 @@
 import { SpaceBaseScene } from "../index";
+import { Planet } from './planet';
 
 export class PlanetMenu extends Phaser.GameObjects.Container {
-    constructor(scene: SpaceBaseScene, x: number, y: number) {
-        super(
-            scene,
-            scene.game.canvas.width / 2,
-            scene.game.canvas.height / 2
-        );
+    private containerBackground: Phaser.GameObjects.Rectangle;
+    private menuText: Phaser.GameObjects.Text;
+    private fontStyle = { font: "20px Mono", fill: "#ddd" };
+
+    constructor(scene: SpaceBaseScene, x: number, y: number, parent: Planet) {
+        super(scene, x, y);
+        this.containerBackground = scene.add.rectangle(0, 0, 0, 0, 0x000);
+        const text = parent.texture.key.toString() + " " + parent.id;
+        this.menuText = scene.add.text(0, 0, text, this.fontStyle);
+
+        this.setPosition(x, y);
+        this.add(this.containerBackground);
+        this.add(this.menuText);
+        this.setVisible(false);
+
+        this.setSize(this.menuText.displayWidth, this.menuText.displayHeight);
+        this.containerBackground.setSize(this.menuText.displayWidth, this.menuText.displayHeight);
+        this.containerBackground.setStrokeStyle(2, 0xf0c0a0);
+
+        scene.add.existing(this);
     }
 }
