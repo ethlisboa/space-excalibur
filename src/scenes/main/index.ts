91900bf072da447c69f3c11f064439f36803fa17
Marin Petrunić <mpetrunic@users.noreply.github.com>
2021-10-23 12:59:11 +0100

    planet interaction (#10)
    * change cursor type
    
    * add planets

--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -1,4 +1,4 @@
-import { Scene } from 'phaser';
+import { Input, Scene } from 'phaser';
 
 export class BaseMapScene extends Scene {
   private map?: Phaser.Tilemaps.Tilemap;
@@ -19,8 +19,8 @@ export class BaseMapScene extends Scene {
 
   preload() {
     this.load.image('avatar', 'assets/sprites/avatar.png')
+    this.load.image('planet-green', 'assets/sprites/planet_green.png')
     this.load.image('space-tileset', 'assets/map/Tilesets/space-tileset.png');
-    this.load.image('planet', 'assets/map/Tilesets/planet.png');
     this.load.tilemapTiledJSON('map', 'assets/map/map.json');
   }
 
@@ -29,13 +29,22 @@ export class BaseMapScene extends Scene {
     // Input Keys
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Background
-    this.map = this.make.tilemap({key: 'map', tileWidth: 50, tileHeight: 50});
+    this.map = this.make.tilemap({ key: 'map', tileWidth: 50, tileHeight: 50 });
     const spaceTileset = this.map.addTilesetImage('space', 'space-tileset')
-    const planetsTileset = this.map.addTilesetImage('planet', 'planet')
-    const spaceLayer = this.map.createLayer("space", spaceTileset, 0, 0);
-    const planetLayer = this.map.createLayer("planets", planetsTileset, 0, 0);
-    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.map.createLayer("space", spaceTileset, 0, 0);
+
+    const planetGroup = this.add.group();
+    for (let i = 0; i < 100; i++) {
+      const planetSprite = this.make.sprite({ 
+        key: 'planet-green',
+        x: Phaser.Math.Between(0, this.map.widthInPixels),
+        y: Phaser.Math.Between(0, this.map.heightInPixels) 
+      }, true).setInteractive({
+        useHandCursor: true
+      });
+      planetSprite.on(Input.Events.POINTER_DOWN, () => console.log("planet clicked"))
+      planetGroup.add(planetSprite);
+    }
 
     // Avatar
     this.avatar = this.physics.add.sprite(
