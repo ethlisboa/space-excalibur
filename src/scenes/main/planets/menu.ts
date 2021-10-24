f01ad4e43183b903bdd15ca3708feef49d483114
Marin Petrunić <mpetrunic@users.noreply.github.com>
2021-10-24 05:08:01 +0100

    Collecting items (#33)
    * fetch planets from the contracts
    
    * get available resources
    
    * collect resources
    
    Co-authored-by: /raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>

--- src/scenes/main/planets/menu.ts
+++ src/scenes/main/planets/menu.ts
@@ -5,12 +5,13 @@ export class PlanetMenu extends Phaser.GameObjects.Container {
     private containerBackground: Phaser.GameObjects.Rectangle;
     private menuText: Phaser.GameObjects.Text;
     private fontStyle = { font: "20px Mono", fill: "#ddd" };
+    private planet: Planet;
 
     constructor(scene: SpaceBaseScene, x: number, y: number, parent: Planet) {
         super(scene, x, y);
+        this.planet = parent;
         this.containerBackground = scene.add.rectangle(0, 0, 0, 0, 0x000);
-        const text = parent.texture.key.toString() + " " + parent.id;
-        this.menuText = scene.add.text(0, 0, text, this.fontStyle);
+        this.menuText = scene.add.text(0, 0, this.getText("unknown"), this.fontStyle);
 
         this.setPosition(x, y);
         this.add(this.containerBackground);
@@ -22,5 +23,13 @@ export class PlanetMenu extends Phaser.GameObjects.Container {
         this.containerBackground.setStrokeStyle(2, 0xf0c0a0);
 
         scene.add.existing(this);
+    }
+
+    public setResources(x: string): void {
+        this.menuText.setText(this.getText(x))
+    }
+
+    private getText(amount: string): string {
+        return `${this.planet.texture.key.toString()} ${this.planet.id} (Available resource: ${amount})`
     }
 }
--- src/scenes/main/planets/planet.ts
+++ src/scenes/main/planets/planet.ts
@@ -1,12 +1,17 @@
+import { getWeb3Provider, getWeb3Signer } from '../../../actions/web3';
 import { Input } from 'phaser';
 import { SpaceBaseScene } from '../index';
 import { PlanetMenu } from './menu';
+import { Contract } from 'ethers';
+import { Planet as PlanetContract } from 'spacexcalibur-contracts/typechain/Planet';
+
+const { abi: PlanetABI, address: planetAddress } = require('spacexcalibur-contracts/deployments/optimistic/Planet.json');
 
 export class Planet extends Phaser.GameObjects.Sprite {
     public id: string;
     public infoBox: PlanetMenu;
 
-    constructor(scene: SpaceBaseScene, x: number, y: number, texture: string) {
+    constructor(scene: SpaceBaseScene, x: number, y: number, texture: string, id?: string) {
         super(
             scene,
             scene.game.canvas.width / 2,
@@ -20,10 +25,24 @@ export class Planet extends Phaser.GameObjects.Sprite {
         this.setPosition(x, y);
         this.setTexture(texture);
 
-        this.id = this.newId();
+        this.id = id ?? this.newId();
         this.infoBox = new PlanetMenu(scene, this.getBottomRight().x, this.getBottomRight().y, this);
 
-        this.on(Input.Events.POINTER_DOWN, () => this.infoBox.setVisible(!this.infoBox.visible));
+        this.on(Input.Events.POINTER_DOWN, async () => {
+            this.infoBox.setVisible(!this.infoBox.visible)
+            try {
+                const provider = getWeb3Provider();
+                const contract = new Contract(planetAddress, PlanetABI, provider) as PlanetContract;
+                const resourceAmount = await contract.accruedResources("0x36Ca879e11aA582AB92d43547da1EA5cFd57067b", this.id);
+                this.infoBox.setResources(resourceAmount.toString());
+            } catch (e) {
+                console.log(e);
+            }
+
+            const signer = await getWeb3Signer();
+            const contract = new Contract(planetAddress, PlanetABI, signer.getSigner()) as PlanetContract;
+            await contract.collect("0x36Ca879e11aA582AB92d43547da1EA5cFd57067b", this.id);
+        });
     }
 
     newId(): string {
